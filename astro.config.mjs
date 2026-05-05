017ff3bd315a764ba8f995ef44cf2417816e490f
devagrooficial <a2agro.oficial@gmail.com>
2026-05-05 12:43:56 -0400

    chore: add site url for render deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
 
 export default defineConfig({
+  site: 'https://mqaccessapp.onrender.com',
   output: 'server',
   adapter: node({
     mode: 'standalone',
