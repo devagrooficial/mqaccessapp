7b0c05f754812f338b9fc2308fce2cb5f6b753b2
devagrooficial <a2agro.oficial@gmail.com>
2026-05-05 16:11:35 -0400

    fix: permanently disable checkOrigin to fix cross-site POST forbidden error on Render

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import node from '@astrojs/node';
 export default defineConfig({
   site: 'https://mqaccessapp.onrender.com',
   security: {
-    checkOrigin: true,
+    checkOrigin: false,
   },
   output: 'server',
   adapter: node({
